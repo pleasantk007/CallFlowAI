3917ddbf0fd56c488c7017818a6705168f8afb6d
pleasantk007 <pleasantk007@gmail.com>
2025-11-23 13:54:45 -0500

    Create crmClient.js

---
+++ config/config/config/integrations/integrations/integrations/crmClient.js
@@ -0,0 +1,9 @@
+async function saveLeadToCrm(lead) {
+  console.log("Saving lead to CRM:", lead);
+  // TODO: add HubSpot / Airtable / Sheets logic here later
+  return true;
+}
+
+module.exports = {
+  saveLeadToCrm
+};
