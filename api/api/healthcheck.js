782b4d8d4864879f948731fb10b155881fbacfe3
pleasantk007 <pleasantk007@gmail.com>
2025-11-23 15:09:41 -0500

    Update healthcheck.js

--- api/api/healthcheck.js
+++ api/api/healthcheck.js
@@ -1,3 +1,4 @@
-module.exports = async function handler(req, res) {
+
+export default async function handler(req, res) {
   res.status(200).json({ status: "ok", service: "CallFlowAI-Core" });
-};
+}
