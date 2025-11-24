7b8dc04a0625b9bdd1f8110344d6054282612831
pleasantk007 <pleasantk007@gmail.com>
2025-11-24 15:03:09 -0500

    Update incoming-voice.js

--- api/incoming-voice.js
+++ api/incoming-voice.js
@@ -1,4 +1,15 @@
-export default function handler(req, res) {
-  res.status(200).send('Test response: function is working');
-}
+export async function POST(req) {
+  const twiml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Say voice="Polly.Matthew">Please leave a message after the tone.</Say>
+  <Record maxLength="60" action="/api/voicemail-save" method="POST" />
+  <Say voice="Polly.Matthew">We did not receive a recording. Goodbye.</Say>
+</Response>`;
 
+  return new Response(twiml, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/xml',
+    },
+  });
+}
