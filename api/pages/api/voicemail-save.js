5bf6a3b4ac4804e699a43bfdcf4cf4b93fceb359
pleasantk007 <pleasantk007@gmail.com>
2025-11-23 21:29:25 -0500

    Create voicemail-save.js

---
+++ api/pages/api/voicemail-save.js
@@ -0,0 +1,18 @@
+export async function POST(req) {
+  const formData = await req.formData();
+  const recordingUrl = formData.get("RecordingUrl");
+
+  console.log("Recording saved at:", recordingUrl);
+
+  const twiml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Say voice="Polly.Matthew">Thanks for your message. Goodbye.</Say>
+</Response>`;
+
+  return new Response(twiml, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/xml',
+    },
+  });
+}
